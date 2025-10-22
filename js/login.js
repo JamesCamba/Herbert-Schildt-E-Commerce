5333a7fd64fed93aa6c0af3ab486d02086b45767
JamesCamba <jamescamba69@gmail.com>
2025-10-23 00:07:10 +0800

    Update login API endpoint to production URL

--- js/login.js
+++ js/login.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         try {
-            const response = await fetch("http://127.0.0.1:5000/login", {
+            const response = await fetch("https://herbert-schildt-e-commerce-15.onrender.com/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password })
